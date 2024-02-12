68b72edb1ca57c5b15fc038fd0f5b650fd782b00
SamoraHunter <44898312+SamoraHunter@users.noreply.github.com>
2024-02-12 17:39:10 +0000

    Update .gitignore

--- ml_grid/pipeline/data_percent_missing.py
+++ ml_grid/pipeline/data_percent_missing.py
@@ -1,26 +1,33 @@
-
 import pickle
+import os
 
 
 def handle_percent_missing(local_param_dict, all_df_columns, drop_list):
-    
-    with open('percent_missing_dict.pickle', 'rb') as handle:
+    percent_missing_drop_list = []
+
+    # Check if the file exists
+    if os.path.exists('percent_missing_dict.pickle'):
+        with open('percent_missing_dict.pickle', 'rb') as handle:
             percent_missing_dict = pickle.load(handle)
 
+        percent_missing_threshold = local_param_dict.get('percent_missing')
 
-    percent_missing_threshold = local_param_dict.get('percent_missing')
-    percent_missing_drop_list = []
-    for col in all_df_columns:
-        try:
-            if(percent_missing_dict.get(col)>percent_missing_threshold):
-                percent_missing_drop_list.append(col)
-        except:
-            pass
-            
-    print(f"Identified {len(percent_missing_drop_list)} at > {percent_missing_threshold} threshold")
+        # Iterate through columns
+        for col in all_df_columns:
+            # Try to get the value from the dictionary
+            try:
+                if percent_missing_dict.get(col) > percent_missing_threshold:
+                    percent_missing_drop_list.append(col)
+            except Exception as e:
+                print(f"Error processing column {col}: {e}")
+                pass
+
+        print(
+            f"Identified {len(percent_missing_drop_list)} columns with > {percent_missing_threshold} percent missing data.")
+    else:
+        print("File 'percent_missing_dict.pickle' not found. Returning empty list.")
+
+    # Extend the drop list with identified columns
     drop_list.extend(percent_missing_drop_list)
-        
-        
+
     return drop_list
-
-
