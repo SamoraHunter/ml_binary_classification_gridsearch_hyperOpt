f035c28879d46f1fbbe9aab6914f0173198f5fff
SamoraHunter <samorahunter@hotmail.co.uk>
2024-04-16 10:53:50 +0100

    updated docstring handle_feature_importance_methods

--- ml_grid/pipeline/data_feature_importance_methods.py
+++ ml_grid/pipeline/data_feature_importance_methods.py
@@ -11,6 +11,31 @@ class feature_importance_methods:
     def handle_feature_importance_methods(
         self, target_n_features, X_train, X_test, y_train, X_test_orig
     ):
+        """
+        Handles feature importance methods on the data.
+
+        Parameters
+        ----------
+        target_n_features : int
+            Target number of features to reduce to.
+        X_train : pd.DataFrame
+            Pandas DataFrame of training data X.
+        X_test : pd.DataFrame
+            Pandas DataFrame of testing data X.
+        y_train : pd.Series
+            Pandas Series of training data y.
+        X_test_orig : pd.DataFrame
+            Pandas DataFrame of original testing data X.
+
+        Returns
+        -------
+        X_train : pd.DataFrame
+            Pandas DataFrame of training data X with reduced features.
+        X_test : pd.DataFrame
+            Pandas DataFrame of testing data X with reduced features.
+        X_test_orig : pd.DataFrame
+            Pandas DataFrame of original testing data X with reduced features.
+        """
 
         # can implement further methods here on features
 
