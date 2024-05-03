d806c5e4affc5e75ab17b51c5d461fdd3a51101e
SamoraHunter <samorahunter@hotmail.co.uk>
2024-05-03 18:45:01 +0100

    parameter space validation and fixes

--- ml_grid/pipeline/grid_search_cross_validate.py
+++ ml_grid/pipeline/grid_search_cross_validate.py
@@ -10,6 +10,7 @@ from IPython.display import clear_output
 from numpy import absolute, mean, std
 from scikeras.wrappers import KerasClassifier
 from sklearn import metrics
+from IPython.display import display
 
 # from sklearn.utils.testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
@@ -81,6 +82,8 @@ class grid_search_crossvalidate:
         if "XGBClassifier" in method_name.lower():
             grid_n_jobs = 1
 
+        
+
         self.metric_list = self.global_params.metric_list
 
         self.error_raise = self.global_params.error_raise
@@ -103,6 +106,10 @@ class grid_search_crossvalidate:
         self.X_test_orig = self.ml_grid_object_iter.X_test_orig
 
         self.y_test_orig = self.ml_grid_object_iter.y_test_orig
+
+        if "svc" in method_name.lower():
+            self.X_train = scale_data(self.X_train)
+            self.X_test = scale_data(self.X_test)
 
         self.cv = RepeatedKFold(
             n_splits=min(10, len(self.X_train)), n_repeats=3, random_state=1
@@ -179,22 +186,27 @@ class grid_search_crossvalidate:
 
             else:
                 print(f"parameter grid size: Full: {pg}")
+
         grid.fit(self.X_train, self.y_train)
 
-        # Get cross validated scores for best hyperparameter model on x_train_/y_train
-        # if type(grid.estimator) is not KerasClassifier:
+
 
         current_algorithm = grid.best_estimator_
         current_algorithm.fit(self.X_train, self.y_train)
 
-        # else:
-        #     current_algorithm = KerasClassifier(
-        #         build_fn=kerasClassifier_class.create_model(),  # dual function definition...in model class.
-        #         verbose=0,
-        #         layers=grid.best_params_["layers"],
-        #         width=grid.best_params_["width"],
-        #         learning_rate=grid.best_params_["learning_rate"],
-        #     )
+        metric_list = self.metric_list
+
+        # Catch only one class present AUC not defined:
+        
+        #dummy_auc_scorer = make_scorer(dummy_auc)
+        if len(np.unique(self.y_train)) < 2:
+            raise ValueError("Only one class present in y_train. ROC AUC score is not defined in that case. grid_search_cross_validate>>>cross_validate")
+
+        if self.global_parameters.verbose >= 1:
+            print("Getting cross validation scores")
+            print(self.X_train.shape, self.y_train.shape)
+            print("y_train value counts:")
+            print(self.y_train.value_counts())
 
         scores = cross_validate(
             current_algorithm,
@@ -204,14 +216,15 @@ class grid_search_crossvalidate:
             cv=self.cv,
             n_jobs=grid_n_jobs,  # Full CV on final best model #exp -1 was 1
             pre_dispatch=80,  # exp,
-            error_score=np.nan,
+            error_score='raise',
+            #error_score=np.nan,
         )
         current_algorithm_scores = scores
         #     scores_tuple_list.append((method_name, current_algorithm_scores, grid))
 
         if self.global_parameters.verbose >= 4:
 
-            debug_print_statements_class.debug_print_scores(scores)
+            debug_print_statements_class(scores).debug_print_scores()
 
         plot_auc = False
         if plot_auc:
@@ -238,3 +251,52 @@ class grid_search_crossvalidate:
 
 
 #         when to use validation set... and how to store which cases are in this valid set? can withold valid set even earlier...? should?
+
+
+def dummy_auc():
+    """
+    Dummy function to return a constant AUC score of 0.5.
+    
+    Parameters:
+    - y_true : array-like of shape (n_samples,)
+        True binary labels.
+    - y_pred : array-like of shape (n_samples,)
+        Target scores, can either be probability estimates or confidence values.
+        
+    Returns:
+    - auc_score : float
+        Constant AUC score of 0.5.
+    """
+    return 0.5
+
+# Create a scorer using make_scorer
+#dummy_auc_scorer = make_scorer(dummy_auc)
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+def scale_data(X_train):
+    """
+    Scale the data to [0, 1] range if it's not already scaled.
+    
+    Args:
+        X_train (pd.DataFrame): Training features.
+        
+    Returns:
+        pd.DataFrame: Scaled training features.
+    """
+    # Initialize MinMaxScaler
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    
+    # Check if data is already scaled
+    min_val = X_train.min().min()
+    max_val = X_train.max().max()
+    
+    # If data is not scaled, then scale it
+    if (min_val < 0 or max_val > 1):
+        # Fit and transform the data
+        X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
+        return X_train_scaled
+    else:
+        # If data is already scaled, return it as is
+        return X_train
