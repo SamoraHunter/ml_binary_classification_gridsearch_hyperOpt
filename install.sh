47cbfe3fa98c7aa44832423f1dc0bcd16d64b4ab
SamoraHunter <samorahunter@hotmail.co.uk>
2025-01-30 09:09:25 +0000

    removed python version check

--- install.sh
+++ install.sh
@@ -7,20 +7,10 @@ print_error_and_exit() {
     exit 1
 }
 
-# Check if Python 3 is found
-if ! command -v python3 &> /dev/null; then
-    print_error_and_exit "Python 3 is not found. Please make sure Python 3 is installed."
-fi
-
-# Check if python3-venv package is installed
-if ! dpkg -s python3-venv &> /dev/null; then
-    print_error_and_exit "The 'python3-venv' package is not installed. Please install it using your package manager (e.g., sudo apt-get install python3-venv)."
-fi
-
 # Check if virtual environment exists
 if [ ! -d "ml_grid_env" ]; then
     # Create virtual environment
-    python3 -m venv ml_grid_env || print_error_and_exit "Failed to create virtual environment"
+    python -m venv ml_grid_env || print_error_and_exit "Failed to create virtual environment"
 fi
 
 # Activate virtual environment
