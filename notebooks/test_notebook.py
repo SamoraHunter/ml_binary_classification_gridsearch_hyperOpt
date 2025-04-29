60d3b8e77b091d11b8a776a41222ee9bbeb006b1
SamoraHunter <samorahunter@hotmail.co.uk>
2025-04-29 14:26:07 +0100

    initial git hook pre push and notebook test

---
+++ notebooks/test_notebook.py
@@ -0,0 +1,11 @@
+import pytest
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+
+def test_notebook():
+    with open("notebooks/unit_test_synthetic.ipynb") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name="ml_grid_env")
+    ep.preprocess(nb)  # Will raise an error if the notebook fails
