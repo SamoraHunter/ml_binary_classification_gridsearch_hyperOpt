19bfdb22b6dcaa98aae287968424d20185ec5261
SamoraHunter <samorahunter@hotmail.co.uk>
2025-10-26 12:11:10 +0000

    minor change

--- notebooks/config_parser.py
+++ notebooks/config_parser.py
@@ -13,7 +13,7 @@ def load_config(config_path: str = 'config.yml'):
         dict: A dictionary containing the configuration.
     """
     # Assume config_path is relative to the project root
-    full_path = Path(__file__).resolve().parents[2] / config_path
+    full_path = Path(__file__).resolve().parents[1] / config_path
     if not full_path.exists():
         raise FileNotFoundError(f"Configuration file not found at: {full_path}")
 
